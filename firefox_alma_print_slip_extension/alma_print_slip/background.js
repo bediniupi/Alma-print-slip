78183229378aefe19655c08724d03447f8c97d2b
Nazzareno Bedini <33749413+bediniupi@users.noreply.github.com>
2019-03-08 13:49:50 +0100

    Delete background.js

--- firefox_alma_print_slip_extension/alma_print_slip/background.js
+++
@@ -1,7 +0,0 @@
-function call_alma_print_slip() {
- browser.tabs.executeScript({
-     file: "alma_print_slip.js"
- });
-}
-
-browser.webRequest.onCompleted.addListener(call_alma_print_slip, {urls: ["*://*.alma.exlibrisgroup.com/*"]});
