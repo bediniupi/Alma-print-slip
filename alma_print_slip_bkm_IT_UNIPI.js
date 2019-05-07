bd7724beab558902428481250a54180b49441b32
Nazzareno Bedini <33749413+bediniupi@users.noreply.github.com>
2019-05-07 13:36:37 +0200

    Bookmarklet version UNIPI

--- alma_print_slip_bkm_IT_UNIPI.js
+++ alma_print_slip_bkm_IT_UNIPI.js
@@ -4,13 +4,13 @@ if (loan_table) {
     var institution_name = "Università di Pisa";
     var institution_logo_link = "https://sbart-unipi.alma.exlibrisgroup.com/infra/branding/logo/logo-email.png?institution=3298&amp;version=1552293706210?version=March2019";
     var body_css = "font-family: Arial, Helvetica, sans-serif;";
-    var table_css = "display: table-row-group !important; border-collapse: collapse;";
-    var institution_css = "order: 0; font-size: medium;";
-    var library_css = "order: 1; font-size: medium;";
-    var title_css = "order: 2; font-size: large;";
-    var user_css = "order: 3; font-size: small; font-weight:bold;";
-    var loans_css = "order: 4; padding-top: 20px;";
-    var signature_css = "order: 5; padding-top:50px;";
+    var table_css = "border-collapse: collapse;";
+    var institution_css = "font-size: medium;";
+    var library_css = "font-size: medium;";
+    var title_css = "font-size: large;";
+    var user_css = "font-size: small; font-weight:bold;";
+    var loans_css = "padding-top: 20px;";
+    var signature_css = "padding-top:50px;";
     var date_format = "it-IT";
     var signature = "Firma _________________________";
     var d = new Date();
@@ -52,8 +52,8 @@ if (loan_table) {
     loan_list_table = loan_list_table.replace(/\n/g, "");
     loan_list_table = "<table border=1>" + loan_list_table + "</table>";
             
-    var css_style = "body {"+body_css+"} table {"+table_css+"} #slip_flex { display: flex; flex-direction: column;} #slip_institution {"+institution_css+"} #slip_library {"+library_css+"} #slip_title {"+title_css+"} #slip_user {"+user_css+"} #slip_loans_table {"+loans_css+"} #slip_signature {"+signature_css+"}";
-    var header_html ="<html><head><meta charset='UTF-8';><style>"+css_style+"</style></head><body><div id='slip_flex'>"; 
+    var css_style = "body {"+body_css+"} table {"+table_css+"} #slip_institution {"+institution_css+"} #slip_library {"+library_css+"} #slip_title {"+title_css+"} #slip_user {"+user_css+"} #slip_loans_table {"+loans_css+"} #slip_signature {"+signature_css+"}";
+    var header_html ="<!DOCTYPE HTML><html><head><meta charset='UTF-8'><style>"+css_style+"</style></head><body><div id='slip'>"; 
     var html_slip = "<div id='slip_institution'><div id='slip_logo'><img src='"+institution_logo_link+"'/></div>"+institution_name+"</div><div id='slip_library'>"+library+"</div><div id='slip_title'>"+slip_name+"</div><div id='slip_user'>"+user_name+" ("+user_id+")</div><div id='slip_loans_table'>"+loan_list_table+"</div><div id='slip_signature'>"+date_slip+"&nbsp;&nbsp;&nbsp;&nbsp;"+signature+"</div></div>";
     var html_div = document.createElement('div');
     html_div.id = "alma_slip_hidden";
